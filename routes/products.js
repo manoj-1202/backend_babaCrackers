50b502e0ed27187cec7e9b28d6281c6fd153e0eb
Manoj <manojpolevault1202@gmail.com>
2025-05-20 20:45:02 +0530

    update

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Product = require('../models/Product');
+const Product = require('../models/product');
 const multer = require('multer');
 const path = require('path');
 
